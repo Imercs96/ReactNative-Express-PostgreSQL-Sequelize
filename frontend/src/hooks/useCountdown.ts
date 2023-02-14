5c04ee35282ccec70e77c210511ec31834bac272
Imercs96 <59058300+Imercs96@users.noreply.github.com>
2023-02-14 16:05:36 -0300

    Feature/token-countdown-timer. Countdown timer hook has been added (#9)

---
+++ frontend/src/hooks/useCountdown.ts
@@ -0,0 +1,45 @@
+import { useEffect, useRef, useState } from 'react';
+
+// Hook Props
+interface Props {
+  goalTime: number;
+}
+
+// Time DataType
+type Time = {
+  days: number; 
+  hours: number; 
+  minutes: number; 
+  seconds: number;
+}
+
+const getReturnValues: (countDown: number) => Time = (countDown) => {
+  // Calculate time left
+  const days: number = Math.floor(countDown / (1000 * 60 * 60 * 24));
+  const hours: number = Math.floor((countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes: number = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds: number = Math.floor((countDown % (1000 * 60)) / 1000);
+
+  return { days, hours, minutes, seconds };
+};
+
+export const useCountdown: ({ ...Props }: Props) => Time = ({ goalTime }) => {
+  // Clock Time
+  const countDownTime: number = Date.now() + goalTime;
+  const currentTime: number = new Date().getTime();
+
+  // Statements
+  const [countDown, setCountDown] = useState<number>(countDownTime - currentTime);
+  const setCountDownRef = useRef<() => void>(() => setCountDown(countDownTime - new Date().getTime()));
+  
+  useEffect(() => {
+    if(countDown <= 0) return;
+    const interval: number = setInterval(setCountDownRef.current, 1000);
+
+    return () => clearInterval(interval);
+  }, [ countDown ]);
+
+  return getReturnValues(countDown);
+};
+
+
