1c258740576e5ed4022b998b0b6f6e30865a5ae2
imercs96 <imercs96@gmail.com>
2023-02-23 11:45:16 -0300

    The background component was built with linear gradient library adding a store main image. In addition, some google fonts were installed and navigation screens were started as Login, Register and Launch. Finally, in Login Screen a form by react-hook-form library was built as example.

--- frontend/src/screens/Launch.tsx
+++ frontend/src/screens/Launch.tsx
@@ -1,6 +1,6 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
-import { View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { Counter } from '../components/Counter';
@@ -13,6 +13,15 @@ export const Launch = ({ navigation }: Props) => {
   return (
     <SafeAreaView>
       <View>
+        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+          <Text> Go to Login </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+          <Text> Go to Register </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+          <Text> Go to Home </Text>
+        </TouchableOpacity>
         <Counter/>
         <Home />
       </View>
